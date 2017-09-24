a848640b6a9301d0ec9ab6237bdb504f326aeacb
radishgz <radishgz@gmail.com>
2017-09-24 08:10:58 +0800

    modify run"

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
-docker run --rm -it -v `pwd`:/srv/jekyll -p 4000:4000 -p 8808:8808  10.0.2.50/xiehq/jekyll   bash
-#bash
+ 
+ docker run --rm -it -v `pwd`:/srv/jekyll --net=host robertxie/robert-gz.github.io  bash
+jekyll build
 
-
-docker run --rm -it -v `pwd`:/srv/jekyll -p 4000:4000 -p 8808:8808  robertxie/robert-gz.github.io bash
 jekyll serve
